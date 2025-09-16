1360a61b994df4a3cb132b54243f9f96f667da5e
Zakoroo <maithemhussein27@gmail.com>
2025-09-16 15:45:36 +0200

    chore: added PR semantics workflow

--- src/test/java/ExampleTest.java
+++
@@ -1,8 +0,0 @@
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.Test;
-
-class ExampleTest {
-    @Test void itWorks() { assertTrue(true); }
-}
-//test commit
