727e54bd677f66ff57d0408c9269e54fd2e4cf66
Zakoroo <maithemhussein27@gmail.com>
2025-09-17 19:13:34 +0200

    fix: removing broken format checker

--- src/main/java/com/ecologicstudios/App.java
+++ src/main/java/com/ecologicstudios/App.java
@@ -1,8 +1,25 @@
 package com.ecologicstudios;
 
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
   public static void main(String[] args) {
-    System.out.println("Hello from ClimateGame runtime!");
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+    Label label = new Label("Hello, JavaFX!");
+    StackPane root = new StackPane(label);
+    Scene scene = new Scene(root, 400, 200);
+
+    primaryStage.setTitle("JavaFX Hello World");
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
 
   public int add(int a, int b) {
