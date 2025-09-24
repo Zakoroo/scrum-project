a9b7b3556184d94842dcdaea8a04e02f63176373
Zakoroo <maithemhussein27@gmail.com>
2025-09-24 23:30:03 +0200

    feat: assign different IDs to alt buttons

--- src/main/java/com/ecologicstudios/client/controllers/GameLoopController.java
+++ src/main/java/com/ecologicstudios/client/controllers/GameLoopController.java
@@ -19,28 +19,35 @@ public class GameLoopController {
     @FXML
     private VBox altBox;
 
-    private List<Button> buttonlist;
+    private List<Button> buttonList;
 
     public void initialize() {
-        buttonlist = new LinkedList<>();
+        buttonList = new LinkedList<>();
 
         try {
             // add buttons to the list
-            buttonlist.add(createButton("alternative 1"));
-            buttonlist.add(createButton("alternative 2"));
+            buttonList.add(createButton("alternative 1"));
+            buttonList.add(createButton("alternative 2"));
 
             // set action for each button in the list
-            buttonlist.forEach(b -> b.setOnAction((e)->handle_answer(e)));
+            buttonList.forEach(b -> b.setOnAction((e)->handle_answer(e)));
+
+            // assign different IDs to all the buttons
+            int i = 0;
+            for (Button b : buttonList) {
+                b.setId(String.format("altBtn%d", i));
+                i++;
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         // add all buttons to the alternatives area
-        altBox.getChildren().addAll(buttonlist);
+        altBox.getChildren().addAll(buttonList);
     }
 
     private void handle_answer(ActionEvent e) {
-        System.out.println("Hello");
+        System.out.println(e.toString());
     }
 
     public Button createButton(String text) throws IOException {
