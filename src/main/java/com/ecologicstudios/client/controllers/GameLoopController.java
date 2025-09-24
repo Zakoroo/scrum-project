393e98bf73f6a0a51950ca483240803fd9205e56
Zakoroo <maithemhussein27@gmail.com>
2025-09-24 18:51:20 +0200

    feat: added event listener to all buttons

--- src/main/java/com/ecologicstudios/client/controllers/GameLoopController.java
+++ src/main/java/com/ecologicstudios/client/controllers/GameLoopController.java
@@ -1,16 +1,16 @@
 package com.ecologicstudios.client.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
 import java.util.*;
-import java.io.IOException.*;
+import java.io.IOException;
 
 public class GameLoopController {
-
-    final private String buttonFxml = "/fxml/altButton.fxml";
+    final private String buttonFxml = "/fxml/altButton.fxml"; // path to the button fxml file
  
     @FXML
     private Label descriptionLabel;
@@ -24,18 +24,25 @@ public class GameLoopController {
         buttonlist = new LinkedList<>();
 
         try {
-            buttonlist.add(createButton("hello"));
-            buttonlist.add(createButton("world"));
+            // add buttons to the list
+            buttonlist.add(createButton("alternative 1"));
+            buttonlist.add(createButton("alternative 2"));
+
+            // set action for each button in the list
+            buttonlist.forEach(b -> b.setOnAction((e)->handle_answer(e)));
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        for(Button b : buttonlist) {
-            altBox.getChildren().add(b);
-        }
+        // add all buttons to the alternatives area
+        altBox.getChildren().addAll(buttonlist);
     }
 
-    public Button createButton(String text) throws Exception{
+    private void handle_answer(ActionEvent e) {
+        System.out.println("Hello");
+    }
+
+    public Button createButton(String text) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(buttonFxml));
         Button button = loader.load();
         button.setText(text);
