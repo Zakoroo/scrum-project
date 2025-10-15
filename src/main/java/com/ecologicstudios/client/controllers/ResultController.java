7809133ffe05d0e3e1e473a7c94d65fbd2c391af
Hussein Hafid <maithemhussein27@gmail.com>
2025-10-15 16:05:07 +0200

    chore: removing unused dependencies

--- src/main/java/com/ecologicstudios/client/controllers/ResultController.java
+++ src/main/java/com/ecologicstudios/client/controllers/ResultController.java
@@ -2,7 +2,6 @@ package com.ecologicstudios.client.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
